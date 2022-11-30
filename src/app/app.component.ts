e809f7efd0f869d6d8f4db3f808168d59ff63776
Konstantin <konstantin@revimedia.com>
2022-12-01 00:57:45 +0200

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ChartData, ChartOptions, ChartType} from 'chart.js';
+
+export interface ExportChart {
+  options: ChartOptions;
+  type: ChartType;
+  data: ChartData;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weather-app';
+  // '2.00,-1.00,0.00,-2.31,1.56,-14.55,-6.73,-3.42,2.17,-0.25,3.73,7.73'
+  chartConfig: ExportChart = {
+    data: {
+      labels: [
+        '2010-01-01', '2011-01-01', '2012-01-01', '2013-01-01',
+        '2014-01-01', '2015-01-01', '2016-01-01', '2017-01-01',
+        '2018-01-01', '2019-01-01', '2020-01-01', '2021-01-01'
+      ],
+      datasets: [
+        {
+          data: [2.00, -1.00, 0.00, -2.31, 1.56, -14.55, -6.73, -3.42, 2.17, -0.25, 3.73, 7.73],
+          label: 'Label A',
+          backgroundColor: 'rgba(255, 255, 0, 0.4)',
+          borderColor: 'yellow',
+          pointBackgroundColor: 'yellow',
+          pointBorderColor: 'yellow',
+          borderWidth: 2
+        }
+      ]
+    },
+    type: 'bar',
+    options: {
+      responsive: true,
+      aspectRatio: 2,
+      scales: {
+        y: {
+          type: 'linear',
+          position: 'left',
+          min: 0,
+          max: 100,
+          ticks: { stepSize: 25 }
+        }
+      },
+      elements: {
+        line: {
+          tension: 0
+        }
+      },
+      plugins: {
+        tooltip: {
+          enabled: true,
+          position: 'nearest'
+        },
+        legend: {
+          display: false,
+          position: 'top'
+        }
+      }
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {ChartjsModule} from "@ctrl/ngx-chartjs";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartjsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
